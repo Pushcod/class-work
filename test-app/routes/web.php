7c9405a0c9d44292cbca818d7a07d5b6abf8a74f
Pushcod <4programart1@gmail.com>
2024-01-18 17:58:15 +0300

    Написали Route для маршуртизации

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -13,12 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 
 //Открытие главной страницы о нас
-Route::get('/about', function (){
-   return view('about');
-});
+//Route::get('/about', function (){
+//   return view('about');
+//});
+
+Route::get('/', [\App\Http\Controllers\FrontendController::class, 'index'])->name('home');
+Route::get('/about', [\App\Http\Controllers\FrontendController::class, 'about'])->name('about');
+
